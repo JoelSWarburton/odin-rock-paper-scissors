df98157e96124a29f83a499550392a319cf98c27
Joel Warburton <Joel.s.warburton@gmail.com>
2023-11-13 22:07:38 +1100

    Modify UI to make game status more clear

--- rock-game.js
+++ rock-game.js
@@ -1,10 +1,12 @@
 
 const buttons = document.querySelectorAll(".button");
 const output = document.querySelector("#output");
-
+const reset = document.querySelector("#reset");
+const roundCounter = document.querySelector("#round-counter");
 
 let playerScore = 0;
 let computerScore = 0;
+let roundCount = 0;
 
 
 buttons.forEach((button) => {
@@ -36,6 +38,8 @@ function getComputerChoice() {
 
 
 function playRound(playerSelection, computerSelection) {
+    roundCount++;
+    roundCounter.textContent = roundCount;
     buttons.forEach((button) => {
         button.classList.remove("selected-border");
         
@@ -51,7 +55,8 @@ function playRound(playerSelection, computerSelection) {
     })
 
     playerSelection = playerSelection.toLowerCase();
-
+    output.classList.remove("loss");
+    output.classList.remove("win");
     if(playerSelection === computerSelection) {
         
         output.textContent = "That round was a draw! You both picked " + playerSelection + ".";
@@ -62,9 +67,10 @@ function playRound(playerSelection, computerSelection) {
     || playerSelection === "paper" && computerSelection === "scissors"
     || playerSelection === "scissors" && computerSelection === "rock") {
         computerScore += 1;
-        let text = "You lose! " + computerSelection + " beats " + playerSelection + "."; 
+        let text = "You lost that round! " + computerSelection + " beats " + playerSelection + "."; 
         console.log(text)
         output.textContent = text;
+        output.classList.add("loss")
     }
 
     //win conditions
@@ -72,7 +78,8 @@ function playRound(playerSelection, computerSelection) {
     || playerSelection === "paper" && computerSelection === "rock" 
     || playerSelection === "scissors" && computerSelection === "paper") {
         playerScore += 1;
-        output.textContent = "You win! " + playerSelection + " beats " + computerSelection + ".";
+        output.textContent = "You won that round! " + playerSelection + " beats " + computerSelection + ".";
+        output.classList.add("win");
     }
 
     updateScores();
@@ -88,7 +95,17 @@ function updateScores() {
 
 
         if(playerScore >= 5 || computerScore >= 5) {
-            document.querySelector("#game-result").textContent = "Game over!"
+            const gameResult = document.querySelector("#game-result");
+            if (playerScore >= 5) {
+                gameResult.textContent = "You're the winner! Congratulations";
+                gameResult.classList.add("win");
+            }
+
+            if (computerScore >= 5) {
+                gameResult.textContent = "Sorry! You lost to the computer."
+                gameResult.classList.add("loss")
+            }
+            
             buttons.forEach((button) => {
                 button.disabled = true;
             })
@@ -97,21 +114,23 @@ function updateScores() {
             restartBtn.setAttribute("id", "restart");
             restartBtn.textContent = "restart"
             restartBtn.addEventListener("click", () => restart());
-            output.appendChild(restartBtn)
+            reset.appendChild(restartBtn)
         }
 }
 
 function restart() {
     playerScore = 0;
     computerScore = 0;
+    roundCount = 0;
+    roundCounter.textContent = roundCount;
     updateScores();
     document.querySelector("#game-result").textContent = "";
    
     const removeBtn = document.querySelector("#restart");
-    output.removeChild(removeBtn);
+    reset.removeChild(removeBtn);
     output.textContent = "";
     buttons.forEach((button) => {
-        console.log("removeing lock")
+        console.log("removing lock")
         button.removeAttribute("disabled")
     })
 }
